059375995ce9d1e07e7fe29c8bc750a06f1d8823
paulwongx <20178251+paulwongx@users.noreply.github.com>
2021-03-07 20:18:34 -0500

    Logs date when updating

--- update.js
+++ update.js
@@ -1,6 +1,13 @@
 const fetchGameMaster = require('./fetchGameMaster.js');
 const fetchPopular = require('./fetchPopular.js');
+const fs = require('fs');
 
-fetchGameMaster().then(()=>{
-    fetchPopular();
+fetchGameMaster().then(async ()=>{
+    await fetchPopular();
+}).then(()=>{
+    const json = fs.readFileSync('./popular.json','utf8');
+    data = JSON.parse(json);
+    lastUpdated = data.lastUpdated
+    console.log(lastUpdated);
 }).catch(err => console.error(err));
+
